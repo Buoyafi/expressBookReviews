f7e52fa0d544873f8e3c9929fe42e4281a5c3bd7
Buoyafi <buoyafibm@gmail.com>
2026-06-23 05:26:31 -0400

    Initial commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -74,6 +74,50 @@ public_users.get('/isbn/:isbn',function (req, res) {
         return res.status(404).json({ message: "Book not found" });
     }
 });
+
+// Get book details based on ISBN (existing route)
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+        return res.status(200).json(book);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+});
+
+// Get book by ISBN using Promise callbacks (NEW)
+public_users.get('/promise/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+
+    const getBook = new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) {
+            resolve(book);
+        } else {
+            reject("Book not found");
+        }
+    });
+
+    getBook
+        .then((book) => {
+            return res.status(200).json(book);
+        })
+        .catch((err) => {
+            return res.status(404).json({ message: err });
+        });
+});
+
+// Get book by ISBN using async/await with Axios (NEW)
+public_users.get('/async/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+    try {
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+        return res.status(200).json(response.data);
+    } catch (err) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
